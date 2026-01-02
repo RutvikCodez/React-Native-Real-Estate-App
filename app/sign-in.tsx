b1009b4aa5e788978d00a6f4f20fb062be52d8b3
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-01-02 21:33:43 +0530

    use flex layout in sign in page instead in padding and margins

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -14,28 +14,34 @@ const SignIn = () => {
           className="w-full h-4/6"
           resizeMode="contain"
         />
-        <View className="px-10">
-          <Text className="text-base text-center uppercase font-rubik text-black-200">
-            Welcome to ReState
-          </Text>
-          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
-            Let&apos;s Get You Closer to {"\n"}
-            <Text className="text-primary-300">Your Ideal Home</Text>
-          </Text>
-          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
-            Login to ReState with Google
-          </Text>
+        <View className="flex flex-col gap-4">
+          <View className="flex flex-col gap-12">
+            <View className="flex flex-col gap-2">
+              <Text className="text-base text-center uppercase font-rubik text-black-200">
+                Welcome to ReState
+              </Text>
+              <Text className="text-3xl font-rubik-bold text-black-300 text-center">
+                Let&apos;s Get You Closer to {"\n"}
+                <Text className="text-primary-300">Your Ideal Home</Text>
+              </Text>
+            </View>
+            <Text className="text-lg font-rubik text-black-200 text-center">
+              Login to ReState with Google
+            </Text>
+          </View>
           <TouchableOpacity
             onPress={handleLogin}
-            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-4"
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 android:elevation-5"
           >
-            <View className="flex flex-row justify-center items-center">
+            <View className="flex flex-row justify-center items-center gap-2">
               <Image
                 source={icons.google}
                 className="w-5 h-5"
                 resizeMode="contain"
               />
-              <Text className="text-lg font-rubik-medium  text-black-300 ml-2">Continue With Google</Text>
+              <Text className="text-lg font-rubik-medium  text-black-300">
+                Continue With Google
+              </Text>
             </View>
           </TouchableOpacity>
         </View>
